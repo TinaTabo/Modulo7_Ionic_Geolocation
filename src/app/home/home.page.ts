65f55b38d9b79a5fe27b4266d2c40f7b411a9971
Cristina Taboada <cris.taboada98@gmail.com>
2023-07-14 19:21:45 +0200

    reto 2 completo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public latitud_promesa:number = 0;
+  public longitud_promesa:number = 0;
+  public latitud_observable:number = 0;
+  public longitud_observable:number = 0;
+  constructor() {
+  }
 
+  getPosition():void{
+    Geolocation.getCurrentPosition()
+      .then(res =>{
+        console.log(res.coords.latitude, res.coords.longitude);
+        this.latitud_promesa = res.coords.latitude;
+        this.longitud_promesa = res.coords.longitude;
+      })
+  }
+
+  watchPosition():void{
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    }
+    Geolocation.watchPosition(options, res =>{
+      if (res?.coords.latitude != undefined && res?.coords.longitude != undefined) {
+        this.latitud_observable = res.coords.latitude;
+        this.longitud_observable = res.coords.longitude;
+      }
+    });
+
+  }
 }
