c1546ed4c05131b14593194ad393b9a201f3d850
Cristina Taboada <cris.taboada98@gmail.com>
2023-07-14 18:39:34 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Modulo7_Ionic_Geolocation',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
